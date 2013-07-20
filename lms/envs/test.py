d6ef87b1f4e5d06e90b1df35155cc865dbe5e542
dcadams <dcadams@stanford.edu>
2013-07-19 22:57:31 -0700

    Added CME setting to test.py

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -201,5 +201,6 @@ PASSWORD_HASHERS = (
 import openid.oidutil
 openid.oidutil.log = lambda message, level=0: None
 
-# Add app for CME Registration
+################### CME Registration app
 INSTALLED_APPS += ('cme_registration',)
+MITX_FEATURES['USE_CME_REGISTRATION'] = True
