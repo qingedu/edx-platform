e91f8441d96e625cf460fab3a377a4b58042178a
Calen Pennington <cale@edx.org>
2013-07-20 07:45:56 -0400

    Add a manage.py command that understands the extra edX options

---
+++ manage.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import os
+import sys
+import glob2
+from argparse import ArgumentParser
+
+def environments_for_service(service):
+    env_prefix = '{}/envs/'.format(service)
+    modules = glob2.glob("{}**/*.py".format(env_prefix))
+    for module_path in modules:
+        if module_path.endswith('__init__.py'):
+            continue
+        yield module_path[len(env_prefix):-3].replace('/', '.')
+
+def parse_args():
+    parser = ArgumentParser(add_help=False)
+    subparsers = parser.add_subparsers(title='system', description='edx service to run')
+
+    lms = subparsers.add_parser(
+        'lms',
+        help='Learning Management System',
+        add_help=False,
+        usage='%(prog)s [options] ...'
+    )
+    lms.add_argument('-h', '--help', action='store_true', help='show this help method and exit')
+    lms.add_argument(
+        '-e', '--environment',
+        choices=list(environments_for_service('lms')),
+        default='dev',
+        help='Which environment settings module to use')
+    lms.add_argument(
+        '-s', '--service-variant',
+        choices=['lms', 'lms-xml', 'lms-preview'],
+        default='lms',
+        help='Which service variant to run, when using the aws environment')
+    lms.set_defaults(help_string=lms.format_help(), env_base='lms.envs')
+
+    cms = subparsers.add_parser(
+        'cms',
+        help='Studio',
+        add_help=False,
+        usage='%(prog)s [options] ...'
+    )
+    cms.add_argument(
+        '-e', '--environment',
+        choices=list(environments_for_service('cms')),
+        default='dev',
+        help='Which environment settings module to use')
+    cms.add_argument('-h', '--help', action='store_true', help='show this help method and exit')
+    cms.set_defaults(help_string=cms.format_help(), env_base='cms.envs', service_variant='cms')
+
+
+    edx_args, django_args = parser.parse_known_args()
+
+    if edx_args.help:
+        print "edX:"
+        print edx_args.help_string
+        print "Django:"
+
+    return edx_args, django_args
+
+
+if __name__ == "__main__":
+    edx_args, django_args = parse_args()
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", edx_args.env_base + '.' + edx_args.environment)
+    os.environ.setdefault("SERVICE_VARIANT", edx_args.service_variant)
+    if edx_args.help:
+        django_args.insert(0, '--help')
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line([sys.argv[0]] + django_args)
